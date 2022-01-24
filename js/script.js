a07c6bffd11d9f1c7402a4da7a612b4b6ec7adad
kiipariss <98284750+kiipariss@users.noreply.github.com>
2022-01-24 20:40:07 +0300

    two

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log('hello')
+
+function ibg(){
+    let ibg=document.querySelectorAll(".ibg");
+for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+        ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+    }
+}
+} 
+ibg();
+
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
